4ddcaee1e1c57ac2668aac1c25be06dba586c257
jaswanth tata <jaswanthtata@gmail.com>
2017-12-26 21:40:53 +0530

    testing display test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ restService.post("/echo", function(req, res) {
       : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
-    displayText: speech,
+    displayText: "i am fine",
     source: "webhook-echo-sample"
   });
 });
